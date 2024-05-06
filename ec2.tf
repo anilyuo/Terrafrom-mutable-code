33b951f0c9b27dbce3df63f520940de3356d851f
anilyuo <168365194+anilyuo@users.noreply.github.com>
2024-05-06 20:36:27 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,39 @@
+# ASG with Launch template
+resource "aws_launch_template" "sh_ec2_launch_templ" {
+  name_prefix   = "sh_ec2_launch_templ"
+  image_id      = "ami-0f1545b0ad649824b" #specific for each region
+  instance_type = "t2.micro"
+  #user_data     = filebase64("user_data.sh")
+
+  network_interfaces {
+    associate_public_ip_address = false
+    subnet_id                   = aws_subnet.sh_subnet_2.id
+    security_groups             = [aws_security_group.sh_sg_for_ec2.id]
+  }
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "demo-instance"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "sh_asg" {
+  # no of instances
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  # source
+  target_group_arns = [aws_lb_target_group.sh_alb_tg.arn]
+
+  vpc_zone_identifier = [ # use private subnet
+    aws_subnet.sh_subnet_2.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.sh_ec2_launch_templ.id
+    version = "$Latest"
+  }
+}
